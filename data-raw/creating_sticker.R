7579dd752edeada2a96c5c65962abc1dd710dacf
goodekat <kgoode@iastate.edu>
2019-03-14 14:00:50 -0500

    changed colors of sticker

--- data-raw/creating_sticker.R
+++ data-raw/creating_sticker.R
@@ -8,15 +8,16 @@ sticker <- sticker(expression(plot.new()),
                    package = "lab3group3",
                    p_x = 1,
                    p_y = 1,
-                   p_color = "lightseagreen",
-                   p_size = 16,
+                   p_color = "wheat1",
+                   p_size = 11,
                    p_family	= "mono",
-                   h_fill = "lemonchiffon",
-                   h_color = "orange1",
+                   h_fill = "midnightblue",
+                   h_color = "goldenrod1",
                    h_size = 3,
                    url = "https://github.com/yaweige/lab3group3",
-                   u_size = 2.5,
-                   u_family = "mono")
+                   u_size = 1.5,
+                   u_family = "mono",
+                   u_color = "white")
 
 # Save the sticker
 ggplot2::ggsave(file = "./data-raw/sticker.png", sticker, width = 3.5, height = 3.5)
