eabf564936f442ef5d34e880baccc0515bb8d2e0
goodekat <kgoode@iastate.edu>
2019-03-14 10:13:52 -0500

    documented the puerto rico dataset

---
+++ R/puerto_rico.R
@@ -0,0 +1,10 @@
+#' Shape file for the country of Puerto Rico
+#'
+#' A shape file that contains the location information of polygons
+#' that make up Puerto Rico.
+#'
+#' @format A shape file with a geometry type of multipolygon with dimensions of XY.
+#'
+#' @source \url{https://gadm.org/download_country_v3.html}
+
+"puerto_rico"
